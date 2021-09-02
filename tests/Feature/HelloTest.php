ec296b2efe76972cb905685772ce24544cc9611e
Sarah-Hans <leawine@gmail.com>
2021-09-02 16:18:04 +0200

    premier commit

---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/test');
+        $response->assertStatus(200);
+        $response->assertSee('Hello World', $escaped = true);
+    }
+}
